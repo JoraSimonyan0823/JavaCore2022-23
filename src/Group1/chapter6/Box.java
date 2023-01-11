ce0babdf1287157769689eebd3de5d925dc3330f
JoraSimonyan0823 <jor.simonyan23.08@gmail.com>
2023-01-11 21:41:03 +0400

    Homework6

---
+++ src/Group1/chapter6/Box.java
@@ -0,0 +1,12 @@
+package Group1.chapter6;
+
+public class Box {
+    double width;
+    double height;
+    double depth;
+
+    void volume() {
+        System.out.print("Обьем равен ");
+        System.out.println(width * height * depth);
+    }
+}
