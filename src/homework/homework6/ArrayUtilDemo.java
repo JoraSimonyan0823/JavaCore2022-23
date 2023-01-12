0a94122491b8d9804ae76d59aed5dafb8850eb74
JoraSimonyan0823 <jor.simonyan23.08@gmail.com>
2023-01-12 11:20:59 +0400

    Homework6+FigurePainter

--- src/homework/homework6/ArrayUtilDemo.java
+++ src/homework/homework6/ArrayUtilDemo.java
@@ -3,27 +3,18 @@ package homework.homework6;
 public class ArrayUtilDemo {
     public static void main(String[] args) {
         int[] array = {2, 8, 9, 7, -1, 21, 12, 22, 14, 45};
-        ArrayUtil all = new ArrayUtil();
-        ArrayUtil max = new ArrayUtil();
-        ArrayUtil min = new ArrayUtil();
-        ArrayUtil first = new ArrayUtil();
-        ArrayUtil last = new ArrayUtil();
-        ArrayUtil average = new ArrayUtil();
-        ArrayUtil evenCount = new ArrayUtil();
-        ArrayUtil oddCount = new ArrayUtil();
-        ArrayUtil sum = new ArrayUtil();
-        ArrayUtil averageSum = new ArrayUtil();
+        ArrayUtil infoArray = new ArrayUtil();
 
-        all.allOfArray(array);
-        max.maxOfArray(array);
-        min.minOfArray(array);
-        first.firstOfArray(array);
-        last.lastOfArray(array);
-        average.averageOfArray(array);
-        evenCount.evenCountArray(array);
-        oddCount.oddCountArray(array);
-        sum.sumArray(array);
-        averageSum.averageSumArray(array);
+        infoArray.allOfArray(array);
+        infoArray.maxOfArray(array);
+        infoArray.minOfArray(array);
+        infoArray.firstOfArray(array);
+        infoArray.lastOfArray(array);
+        infoArray.averageOfArray(array);
+        infoArray.evenCountArray(array);
+        infoArray.oddCountArray(array);
+        infoArray.sumArray(array);
+        infoArray.averageSumArray(array);
 
     }
 }
--- src/homework/homework6/FigurePainterDemo.java
+++ src/homework/homework6/FigurePainterDemo.java
@@ -2,19 +2,13 @@ package homework.homework6;
 
 public class FigurePainterDemo {
     public static void main(String[] args) {
-        FigurePainter fp1 = new FigurePainter();
-        FigurePainter fp2 = new FigurePainter();
-        FigurePainter fp3 = new FigurePainter();
-        FigurePainter fp4 = new FigurePainter();
-        FigurePainter fp5 = new FigurePainter();
+        FigurePainter fp = new FigurePainter();
 
-
-
-        fp1.figureOne(5, '*');
-        fp2.figureTwo(4, '$');
-        fp3.figureThree(6, '^');
-        fp4.figureFour(5,'%');
-        fp5.figureFive(5,'#');
+        fp.figureOne(5, '*');
+        fp.figureTwo(4, '$');
+        fp.figureThree(6, '^');
+        fp.figureFour(5, '%');
+        fp.figureFive(5, '#');
 
     }
 }
